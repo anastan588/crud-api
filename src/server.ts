e0ff62c6bf06d8384c95530ff5a9c45c59468a3f
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-02-14 00:10:37 +0300

    feat: add error handling for server and for wrong endpoints

--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,19 @@ export const port = process.env.PORT || 3000;
 console.log(process.env.PORT);
 
 export const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url.startsWith('/api/users')) {
-    createUser(req, res);
-  }else  if (req.method === 'GET' && req.url.startsWith('/api/users')) {
-    getUsers(req, res);
-  } 
-  else {
-    // Handle other routes or methods
-    res.statusCode = 404;
-    res.end('Not Found');
+  try {
+    if (req.method === 'POST' && req.url.startsWith('/api/users')) {
+      createUser(req, res);
+    } else if (req.method === 'GET' && req.url.startsWith('/api/users')) {
+      getUsers(req, res);
+    } else {
+      // Handle other routes or methods
+      res.statusCode = 404;
+      res.end('Not Found');
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ message: 'Internal Server Error' }));
   }
 });
